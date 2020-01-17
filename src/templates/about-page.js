d3420994d489f04b69d168ba1d18a864e4c251fc
Robin Blixter <r.blixter89@gmail.com>
2020-01-17 09:20:10 +0100

    reworked the about-page template

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -2,55 +2,45 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
-import Employees from "../components/Employees"
+import Employees from '../components/Employees'
 // import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 // import Content, { HTMLContent } from '../components/Content'
 
 export const AboutPageTemplate = ({
-  image,
   title,
   heading,
   description,
   employees,
 }) => (
-  <div className="content">
-    <div
-      className="full-width-image-container margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-      }}
-    >
-      <h2
-        className="has-text-weight-bold is-size-1"
-        style={{
-          boxShadow:
-          'rgb(46,52,64) 0.5rem 0px 0px, rgb(46,52,64) -0.5rem 0px 0px',
-          backgroundColor: 'rgb(46,52,64)',
-          color: 'rgb(229,233,240)',
-          padding: '1rem',
-        }}
-      >
-        {title}
-      </h2>
-    </div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
+  <div>
+    <section className='hero is-primary is-bold'>
+      <div className='hero-body'>
+        <div className='container'>
+          <div className='columns'>
+            <div className='column is-10 is-offset-1'>
+              <div className='section'>
+                <h1 className='title has-text-centered'>{title}</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className='section section--gradient'>
+      <div className='container'>
+        <div className='section'>
+          <div className='columns'>
+            <div className='column is-10 is-offset-1'>
+              <h3 className='has-text-weight-semibold is-size-2'>{heading}</h3>
               <p>{description}</p>
             </div>
           </div>
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <h2 className="has-text-centered">The Employees</h2>
+          <div className='columns'>
+            <div className='column is-10 is-offset-1'>
+              <h2 className='has-text-centered has-text-weight-semibold is-size-4'>The Employees</h2>
               <Employees gridItems={employees.information} />
-              <div className="columns">
-                <div className="column is-7">
-                </div>
+              <div className='columns'>
+                <div className='column is-7' />
               </div>
             </div>
           </div>
@@ -61,7 +51,6 @@ export const AboutPageTemplate = ({
 )
 
 AboutPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   title: PropTypes.string,
   heading: PropTypes.string,
   description: PropTypes.string,
@@ -76,7 +65,6 @@ const AboutPage = ({ data }) => {
   return (
     <Layout>
       <AboutPageTemplate
-        image={frontmatter.image}
         title={frontmatter.title}
         heading={frontmatter.heading}
         description={frontmatter.description}
@@ -97,13 +85,6 @@ export const aboutPageQuery = graphql`
     markdownRemark(id: { eq: $id }) {
       frontmatter {
         title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
         heading
         description
         employees {
